a467d6ac48118ca699bd0fce321ac6326c074158
Elia Bieri <contact@eliabieri.com>
2020-02-19 16:11:47 +0100

    added packages in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pyaare',
-    version='1.0.1',
+    version='1.0.2',
     description='A Python wrapper around the aare.guru API',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -26,7 +26,8 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
 
-    packages=find_packages(where='lib'),
+    packages=find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     python_requires='>=3.7',
     install_requires=['requests'],
 )
